01f02f5d7961529517e424bd02157198f740a800
Aaron Chen <aaron.chen@my.uwrf.edu>
2023-02-09 00:18:54 -0600

    updating the code

---
+++ main.py
@@ -0,0 +1,35 @@
+import string
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import url_for, request
+import random
+
+app = Flask(__name__)
+current_lobies = {}
+
+
+@app.route("/")
+def hello_world():
+    return render_template("game.html")
+
+
+@app.get("/game?room=<roomId>")
+def game(roomId):
+    current_lobies[roomId]
+    GameRoom = current_lobies[roomId]
+    return f"Post {GameRoom}"
+
+
+@app.get("/lobby")
+def signup_form():
+    return render_template("room_form.html")
+
+
+@app.post("/lobby")
+def post_form():
+    randomID = "".join(random.choices(string.ascii_letters + string.digits, k=10))
+    players = []
+    players.append(request.form["name"])
+    current_lobies[randomID] = players
+    return redirect(url_for("game", roomId=randomID))
