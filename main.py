b813cf10f562c143cc7c5143a436ea3e4d90f90b
Aaron Chen <aaron.chen@my.uwrf.edu>
2023-03-23 21:32:19 -0500

    two players on the same game

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import string
 from flask import Flask
 from flask import render_template
@@ -6,19 +7,32 @@ from flask import url_for, request
 import random
 
 app = Flask(__name__)
-current_lobies = {}
+# current_lobies = {}
+players = []
+game_state = {
+    "board": [[" " for _ in range(7)] for _ in range(6)],
+    "currentPlayer": "R",
+    "gameOver": False,
+    "currentColumns": [5, 5, 5, 5, 5, 5, 5],
+}
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hellowoirld():
+    return "<h1> hellow world!!!</h1>"
 
 
-@app.get("/game?room=<roomId>")
-def game(roomId):
-    current_lobies[roomId]
-    GameRoom = current_lobies[roomId]
-    return f"Post {GameRoom}"
+@app.get("/game")
+def single_game():
+    return render_template("game.html", players=players, game_state=game_state)
+
+
+@app.post("/game")
+def update_single_game():
+    # nice security "feature"
+    global game_state
+    game_state = request.get_json()
+    return ("", 204)
 
 
 @app.get("/lobby")
@@ -28,8 +42,15 @@ def signup_form():
 
 @app.post("/lobby")
 def post_form():
-    randomID = "".join(random.choices(string.ascii_letters + string.digits, k=10))
-    players = []
-    players.append(request.form["name"])
-    current_lobies[randomID] = players
-    return redirect(url_for("game", roomId=randomID))
+    # randomID = "".join(random.choices(string.ascii_letters + string.digits, k=10))
+
+    global players
+    global game_state
+    players = [request.form["name"]]
+    game_state = {
+        "board": [[" " for _ in range(7)] for _ in range(6)],
+        "currentPlayer": "R",
+        "gameOver": False,
+        "currentColumns": [5, 5, 5, 5, 5, 5],
+    }
+    return redirect(url_for("single_game"))
---
+++ static/connect4.js
@@ -0,0 +1,110 @@
+var playerRed ="R";
+var playerYellow = "Y";
+/*
+let gameState = {
+    board: [],
+    currentPlayer: playerRed,
+    gameOver: false,
+    currentColumns: null
+}
+*/
+var number_rows = 6;
+var number_columns = 7;
+
+window.onload = function(){
+    setGame()
+}
+function setGame() {
+    for(let r=0; r<number_rows; r++){
+        for(let c=0; c<number_columns;c++) {
+            
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click",setPiece);
+            if (gameState.board[r][c] == "R") {
+                tile.classList.add("red-piece");
+            } else if (gameState.board[r][c] == "Y") {
+                tile.classList.add("yellow-piece");
+            }
+            document.getElementById("board").append(tile);
+        }
+    }
+}
+async function setPiece(){
+    console.log(gameState)
+    if(gameState.gameOver){
+        return;
+    }
+
+    let coords=this.id.split("-");
+    let r =parseInt(coords[0]);
+    let c =parseInt(coords[1]);
+    
+    r = gameState.currentColumns[c];
+    if(r<0){
+        return;
+    }
+    gameState.board[r][c] = gameState.currentPlayer;
+    let tile = document.getElementById(r.toString() +"-" + c.toString());
+    if(gameState.currentPlayer == playerRed) {
+        tile.classList.add("red-piece");
+        gameState.currentPlayer = playerYellow;
+    }
+    else{
+        tile.classList.add("yellow-piece");
+        gameState.currentPlayer = playerRed;
+    }
+    r-=1;
+    gameState.currentColumns[c] = r;
+
+    checkWinner();
+    await fetch(window.location.href, { method: "POST",
+                                        headers: {"Content-Type": "application/json"},
+                                        body: JSON.stringify(gameState)})
+}
+
+function checkWinner(){
+    //horizontallly
+    for(let r=0; r<number_rows; r++){
+        for(let c=0; c<number_columns-3;c++){
+            if(gameState.board[r][c]==gameState.board[r][c+1] && gameState.board[r][c+1]==gameState.board[r][c+2] && gameState.board[r][c+2] == gameState.board[r][c+3]){
+                if(gameState.board[r][c] != ' '){
+                    setWinner(r,c);
+                    return;
+                }
+            }
+        }
+    }
+    //vertically
+    for(let c=0; c<number_columns;c++){
+        for(let r=0; r<number_rows-3; r++){
+            if(gameState.board[r][c]==gameState.board[r+1][c] && gameState.board[r+1][c]==gameState.board[r+2][c] && gameState.board[r+2][c] == gameState.board[r+3][c]){
+                if(gameState.board[r][c] != ' '){
+                    setWinner(r,c);
+                    return;
+                }
+            }
+        }
+    }
+    //diagonally
+    for(let c=0; c<number_columns-3;c++){
+        for(let r=0; r<number_rows-3; r++){
+            if(gameState.board[r][c]==gameState.board[r+1][c+1] && gameState.board[r+1][c+1]==gameState.board[r+2][c+2] && gameState.board[r+2][c+2] == gameState.board[r+3][c]){
+                if(gameState.board[r][c] != ' '){
+                    setWinner(r,c);
+                    return;
+                }
+            }
+        }
+    }
+}
+function setWinner(r,c) {
+    let winner = document.getElementById("winner");
+    if(gameState.board[r][c] == playerRed){
+        winner.innerText = "Red Wins";
+    }
+    else{
+        winner.innerText = "Yellow Wins"
+    }
+}
